5f4dab83442db0518afbe1c7c0215ce08f10a4e9
Erica Cannedy <encannedy@gmail.com>
2020-05-07 14:50:09 -0400

    js created for beef, chicken, pork and seafood.  beef semi working

---
+++ ChickenMeals.js
@@ -0,0 +1,28 @@
+$(document).ready(function () {
+    function getMeal1 () {
+      var queryURL = "https://www.themealdb.com/api/json/v1/1/filter.php?c=Chicken&apikey=1";
+  //     var globalMeals=[];
+  //     var randomMeal= globalMeals[Math.floor(Math.random() * globalMeals.length)];
+  // console.log("this is the random meal" + randomMeal);
+  // var globalMeals=[];
+  
+      $.ajax({
+        url: queryURL,
+        method: "GET",
+      }).then(function (response) {
+        console.log(response);
+      var Meals= response.meals;  
+      var randomMeal=response.meals[Math.floor(Math.random() *Meals.length)]
+         var mealImg= response.meals[0].strMealThumb;
+         console.log(mealImg);
+        var mealId= response.meals[0].stridMeal;
+        var mealTitle= response.meals[0].strMeal;
+        $("#meal1Title").text(mealTitle);
+        var imageEl = $("<img>");
+        imageEl.attr("src", mealImg);
+        $("#meal1img").append(imageEl);
+      });
+    };
+    getMeal1();
+  });
+  //need second api for full description by ID https://www.themealdb.com/api/json/v1/1/lookup.php?i=" + mealId
